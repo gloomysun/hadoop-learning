ce5e86781559bb2374e92ea985b968679620560f
ly <409575046@qq.com>
2018-10-09 22:57:44 +0800

    kafka api

---
+++ 05-kafka/生产者api/src/main/java/cn/ly/callBackProducer/CallBackProducer.java
@@ -0,0 +1,38 @@
+package cn.ly.callBackProducer;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class CallBackProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        // Kafka服务端的主机名和端口号
+        props.put("bootstrap.servers", "vm2:9092");
+        // 等待所有副本节点的应答
+        props.put("acks", "all");
+        // 消息发送最大尝试次数
+        props.put("retries", 0);
+        // 一批消息处理大小
+        props.put("batch.size", 16384);
+        // 请求延时
+        props.put("linger.ms", 1);
+        // 发送缓存区内存大小
+        props.put("buffer.memory", 33554432);
+        // key序列化
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // value序列化
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("third", Integer.toString(i), "test" + i), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    System.out.println(metadata.offset());
+                }
+            });
+
+        producer.close();
+    }
+}
---
+++ 05-kafka/生产者api/src/main/java/cn/ly/partitionerProducer/PartitionerProducer.java
@@ -0,0 +1,40 @@
+package cn.ly.partitionerProducer;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class PartitionerProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        // Kafka服务端的主机名和端口号
+        props.put("bootstrap.servers", "vm2:9092");
+        // 等待所有副本节点的应答
+        props.put("acks", "all");
+        // 消息发送最大尝试次数
+        props.put("retries", 0);
+        // 一批消息处理大小
+        props.put("batch.size", 16384);
+        // 请求延时
+        props.put("linger.ms", 1);
+        // 发送缓存区内存大小
+        props.put("buffer.memory", 33554432);
+        // key序列化
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // value序列化
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        //自定义分区
+        props.put("partitioner.class","cn.ly.partitionerProducer.CustomPartitioner");
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("third", Integer.toString(i), "test" + i), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    System.out.println(metadata.offset());
+                }
+            });
+
+        producer.close();
+    }
+}
